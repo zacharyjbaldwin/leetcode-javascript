58f86efcbcea78f4c1dc7e345fc1e67ce929fa49
zacharyjbaldwin <zacharyjbaldwin@gmail.com>
2022-01-22 17:14:41 -0600

    Added 28

---
+++ 28-implement-strStr()/28-implement-strStr().js
@@ -0,0 +1,13 @@
+/**
+ * @param {string} haystack
+ * @param {string} needle
+ * @return {number}
+ */
+ var strStr = function(haystack, needle) {
+    if (!needle.length) return 0;
+    if (!haystack.includes(needle)) return -1;
+    
+    return haystack.split(`${needle}`)[0].length;
+};
+
+console.log(strStr('hello', 'lo'));
